14458b7d3a2181697b6c0e6ce1597810461f6438
jearx85 <jearx@hotmail.com>
2024-04-04 11:44:37 -0500

    se elimina paquete obsoleto

--- components/offcanvasMenu/plugin/noteUtils.ts
+++ components/offcanvasMenu/plugin/noteUtils.ts
@@ -1,7 +1,4 @@
 import { v4 as uuidv4 } from 'uuid';
-import markdownIt from 'markdown-it';
-import { useQuery } from 'convex/react';
-import { api } from '@/convex/_generated/api';
 
 export function generateUUID(): string {
   return uuidv4();
