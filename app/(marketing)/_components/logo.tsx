784509a7528f3bc41d73d6ece9554e3ed6a668f6
jearx85 <jearx@hotmail.com>
2024-04-22 10:35:03 -0500

    Se mejora la pagina de login

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -1,33 +1,23 @@
 import Image from "next/image";
-import { Poppins } from "next/font/google";
 
-import { cn } from "@/lib/utils";
-
-const font = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "600"]
-});
 
 export const Logo = () => {
   return (
-    <div className="hidden md:flex items-center gap-x-2">
+    <div >
       <Image
-        src="/icon-512x512.png"
-        height="30"
-        width="40"
+        src="/logo_nadhis.png"
+        height={200}
+        width={200}
         alt="Logo"
         className="dark:hidden"
       />
       <Image
-        src="/icon-512x512-dark.png"
-        height="30"
-        width="40"
+        src="/logo_nadhis-dark.png"
+        height={200}
+        width={200}
         alt="Logo"
         className="hidden dark:block"
       />
-      <p className={cn("font-semibold", font.className)}>
-        Nadhis Digital Brain
-      </p>
     </div>
   )
 }
