a8e5b8c36dff825df523ac5d0907229678b38f20
jearx85 <jearx@hotmail.com>
2024-04-30 16:47:21 -0500

    correcion

--- app/(dashboard)/dashboard/components/DashBoard.tsx
+++ app/(dashboard)/dashboard/components/DashBoard.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Grid from "../../_components/grid/Grid";
 
-import "./Dashboard.css";
+// import "./Dashboard.css";
 import Navbar from "../../_components/navigation/Navbar";
 
 export default function Dashboard() {
