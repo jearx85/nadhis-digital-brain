b254d6d44f0f5ebd7dc86192c626bbad69ccc1fc
jearx85 <jearx@hotmail.com>
2024-05-20 10:38:32 -0500

    Se mejora presentación del bloque de mapas

--- components/myInlineContent/mapBlockContent/MapBlockComponent.tsx
+++ components/myInlineContent/mapBlockContent/MapBlockComponent.tsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Mapa from "../../../public/map.png";
 // import "./SideToSide.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
+
+
+const customIcon = new L.Icon({
+  iconUrl: '/marker-icon.png',
+  iconSize: [38, 38], // Ajusta el tamaño según sea necesario
+  iconAnchor: [19, 38], // Para centrar el icono, ajústalo según sea necesario
+  popupAnchor: [0, -38], // Para ajustar la posición del popup
+});
 
 export default function MapBlockComponent(props: any) {
   function handleClick() {
     alert("Click");
   }
 
+  useEffect(() => {
+    // Configura las rutas de los iconos predeterminados sin acceder a propiedades privadas
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+      iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+      shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+    });
+  }, []);
+  
   return (
     <>
       {/* <div className="flex"> */}
@@ -36,7 +54,9 @@ export default function MapBlockComponent(props: any) {
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://www.google.com/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
             />
-            <Marker position={[6.25184, -75.56359]}>
+            <Marker
+            icon={customIcon}
+              position={[6.25184, -75.56359]}>
               <Popup>
                 A pretty CSS3 popup. <br /> Easily customizable.
               </Popup>
