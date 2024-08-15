4044b971802ed7341150e7ff43d164b66d6377b4
jearx85 <82104527+jearx85@users.noreply.github.com>
2024-08-15 16:37:44 -0500

    Se crea modal para insertar titulos

--- components/innerMenu/addTitle/AddTitle.tsx
+++ components/innerMenu/addTitle/AddTitle.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useBlockNoteEditor } from "@blocknote/react";
 import { generateUUID } from "@/components/offcanvasMenu/plugin/noteUtils";
+import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
 
 export default function AddTitle() {
   const editor = useBlockNoteEditor();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [title, setTitle] = useState(""); 
+  const [lastBlockId, setLastBlockId] = useState(""); 
 
   let idsList: any[] = [];
 
@@ -11,38 +15,74 @@ export default function AddTitle() {
     editor.document.map((block) => {
       idsList.push(block.id);
     });
-    const lasBlockId = idsList[idsList.length - 1];
-    console.log(lasBlockId);
+    setIsModalOpen(true);
+    setLastBlockId(idsList[idsList.length - 1]);
+  }
 
-    editor.insertBlocks(
-      [
-        {
-          id: generateUUID(),
-          type: "heading",
-          props: {
-            textColor: "gray",
-            backgroundColor: "default",
-            textAlignment: "left",
-            level: 2,
-          },
-          content: [
-            {
-              type: "text",
-              text: "INFORME DE GESTIÓN | 2022",
-              styles: {},
+  function handleModalClose() {
+    setIsModalOpen(false); // Cierra el modal
+  }
+
+  function handleSaveTitle() {
+    
+    setIsModalOpen(false); 
+
+    try{
+      editor.insertBlocks(
+        [
+          {
+            id: generateUUID(),
+            type: "heading",
+            props: {
+              textColor: "default",
+              backgroundColor: "default",
+              textAlignment: "left",
+              level: 2,
             },
-          ],
-          children: [],
-        },
-      ],
-      lasBlockId,
-      "after"
-    );
+            content: [
+              {
+                type: "text",
+                text: title, // Usar el título guardado
+                styles: {},
+              },
+            ],
+            children: [],
+          },
+        ],
+        lastBlockId,
+        "after"
+      );
+
+    }catch (error) {
+      console.error("Error insertando bloque de encabezado", error);
+    }
   }
 
   return (
     <div>
-      <button onClick={handleClick}>Agregar titulo</button>
+      <button onClick={handleClick}>Agregar título</button>
+
+      {isModalOpen && (
+        <Dialog open={isModalOpen} onOpenChange={handleModalClose}>
+          <DialogContent>
+            <DialogHeader className="border-b pb-3">
+              <h2 className="text-lg font-medium">Insertar un título</h2>
+            </DialogHeader>
+            <div className="flex flex-col gap-y-1">
+              <input
+                className="p-2"
+                type="text"
+                placeholder="Ingrese su título"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div className="flex justify-end pt-4">
+              <button className="btn border rounded-xl p-2 hover:bg-slate-200" onClick={handleSaveTitle}>Guardar</button>
+            </div>
+          </DialogContent>
+        </Dialog>
+      )}
     </div>
   );
 }
