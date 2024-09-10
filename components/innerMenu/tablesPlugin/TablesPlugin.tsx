570cbb94b8565751816eec9f8188f180b697f361
jearx85 <82104527+jearx85@users.noreply.github.com>
2024-09-10 15:44:08 -0500

    Se crea dialog para insertar tablas

--- components/innerMenu/tablesPlugin/TablesPlugin.tsx
+++ components/innerMenu/tablesPlugin/TablesPlugin.tsx
@@ -1,37 +1,53 @@
-"use client"
-import React, { useState, useEffect} from 'react';
+"use client";
+import React, { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useBlockNoteEditor } from "@blocknote/react";
-import { generateUUID } from '../../offcanvasMenu/plugin/noteUtils';
+import { generateUUID } from "../../offcanvasMenu/plugin/noteUtils";
+import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
 
 interface ApiResponse {
-  id: "string",
-    type: "table",
-    props: {
-      textColor: "default",
-      backgroundColor: "default"
-    },
-    content: {
-      rows: {
-        cells: any[];
-      }[];
-    };
-
+  id: "string";
+  type: "table";
+  props: {
+    textColor: "default";
+    backgroundColor: "default";
+  };
+  content: {
+    rows: {
+      cells: any[];
+    }[];
+  };
 }
 
 const getApiInfo = async () => {
-  const data = await fetch(`http://localhost:8081/ultimos_registros/wazetraffic`).then((res) => res.json());
+  const data = await fetch(
+    `http://localhost:8081/ultimos_registros/wazetraffic`
+  ).then((res) => res.json());
   return data;
 };
 
 export default function TablesComponent() {
   const [scrolled, setScrolled] = useState(false);
-  const [datos, setDatos] = useState<any[]>([]); 
+  const [datos, setDatos] = useState<any[]>([]);
   const [documentId, setDocumentId] = useState<any>();
   const [documentTitle, setDocumentTitle] = useState<string | null>(null);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [lastBlockId, setLastBlockId] = useState("");
+
   const params = useParams();
   const getIdTitle = useMutation(api.documents.getIdTitle);
   const update = useMutation(api.documents.update);
@@ -41,7 +57,7 @@ export default function TablesComponent() {
   const onChange = (content: any) => {
     update({
       id: params.documentId as Id<"documents">,
-      content  // Convert to JSON string
+      content, // Convert to JSON string
     });
   };
 
@@ -51,11 +67,13 @@ export default function TablesComponent() {
 
   useEffect(() => {
     if (documentId) {
-      getIdTitle({ id: documentId as Id<"documents"> }).then((title) => {
-        setDocumentTitle(title);
-      }).catch((error) => {
-        console.error("Error fetching document title: ", error);
-      });
+      getIdTitle({ id: documentId as Id<"documents"> })
+        .then((title) => {
+          setDocumentTitle(title);
+        })
+        .catch((error) => {
+          console.error("Error fetching document title: ", error);
+        });
     }
   }, [documentId, getIdTitle]);
 
@@ -72,60 +90,81 @@ export default function TablesComponent() {
   //   </tr>
   // ));
 
+  function handleModalClose() {
+    setIsModalOpen(false); // Cierra el modal
+  }
+
   const handleClick = () => {
     generateUUID();
 
-    getApiInfo().then((data: ApiResponse) => {
-      setDatos(data.content.rows);
-      const content = [{
-        id: generateUUID(),
-        type: "table",
-        props: {
-          textColor: "default",
-          backgroundColor: "default"
-        },
-        content: {
-          rows: data.content.rows
-        },
-        children: [],
-      }]
-      onChange(content);
-    }).catch((error) => {
-      console.error("Error fetching API data: ", error);
-    });
-  }
+    getApiInfo()
+      .then((data: ApiResponse) => {
+        setDatos(data.content.rows);
+        const content = [
+          {
+            id: generateUUID(),
+            type: "table",
+            props: {
+              textColor: "default",
+              backgroundColor: "default",
+            },
+            content: {
+              rows: data.content.rows,
+            },
+            children: [],
+          },
+        ];
+        onChange(content);
+      })
+      .catch((error) => {
+        console.error("Error fetching API data: ", error);
+      });
+  };
 
+  let idsList: any[] = [];
+
+  function handleModalClick() {
+    editor.document.map((block) => {
+      idsList.push(block.id);
+    });
+    setIsModalOpen(true);
+    setLastBlockId(idsList[idsList.length - 1]);
+  }
 
   return (
     <div>
-      <h1 className="my-2 text-xl">Agregar Tablas</h1>
-      <p>La tabla se agregará en el documento: 
-        <b> { documentTitle } </b>
-      </p>
-      <div className="filtro-categorias d-flex mt-10">
-        <h1>Seleccione indice</h1>
-        <select
-          className="Mydropdown bg-white dark:bg-[#121212]"
-          name="indexList"
-        >
-          <option value="" disabled>
-            Seleccione categoría
-          </option>
-          <option value="Wazealerts">
-            Wazealerts
-          </option>
-          <option value="Wazetraffic">
-            Wazetraffic
-          </option>
-        </select>
-      </div>
-
-      <button className='mt-10 border rounded-lg p-3 hover:bg-slate-300'
-       onClick={handleClick}
-      >
-        Generar tabla
-      </button>
-      
+      <button onClick={handleModalClick}>Agregar Tablas</button>
+      {isModalOpen && (
+        <Dialog open={isModalOpen} onOpenChange={handleModalClose}>
+          <DialogContent>
+            <DialogHeader className="border-b pb-3">
+              <h2 className="text-lg font-medium">
+                La tabla se agregará en el documento:
+                <b> {documentTitle} </b>
+              </h2>
+            </DialogHeader>
+            <div className="filtro-categorias d-flex mt-10">
+              <h1>Seleccione indice</h1>
+              <select
+                className="Mydropdown bg-white dark:bg-[#121212]"
+                name="indexList"
+              >
+                <option value="" disabled>
+                  Seleccione categoría
+                </option>
+                <option value="Wazealerts">Wazealerts</option>
+                <option value="Wazetraffic">Wazetraffic</option>
+              </select>
+            </div>
+            <Button
+              className="mt-10 border rounded-lg p-3"
+              onClick={handleClick}
+            >
+              Generar tabla
+            </Button>
+          </DialogContent>
+        </Dialog>
+      )}
     </div>
-  )
+  );
 }
